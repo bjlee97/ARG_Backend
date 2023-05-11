16bb09acb8fe2f38e413a6c740c11ee6031c5200
Brandon Lee <124757679+bjlee97@users.noreply.github.com>
2023-05-10 21:46:39 -0700

    inventory

---
+++ arg_backend/urls.py
@@ -0,0 +1,29 @@
+
+from rest_framework.routers import DefaultRouter
+from rest_framework import routers
+from django.urls import path
+from django.conf.urls import include
+from django.urls import re_path
+from . import views
+
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('test', views.test, name='test'),
+    path('list_teams',views.get_team_details,name='list_teams'),
+    path('get_puzzles',views.get_puzzles,name='get_puzzles'),
+    path('register',views.register,name='register'),
+    path('get_hint',views.get_hint,name='get_hint'),
+    path('validation',views.validation,name='validation'),
+    path('get_current_puzzle',views.get_current_puzzle,name='get_current_puzzle'),
+    path('get_location_hint',views.get_location_hint,name='get_location_hint'),
+    path('get_image_url',views.get_image_url,name='get_image_url'),
+    path('getSecondHalf',views.getSecondHalf,name='getSecondHalf'),
+    path('add_points_player',views.add_points_player,name='add_points_player'),
+    path('authenticate',views.authenticate_team,name='authenticate'),
+    re_path('add_points/(?P<team_username>[\w.@+-]+)/(?P<points>[\w.@+-]+)$', views.add_points),
+    re_path('get_next_puzzle/(?P<puzzle_id>[\w.@+-]+)$', views.get_next_puzzle),
+    path('createAndServeImage',views.createAndServeImage,name='createAndServeImage'),
+    path('list_inventory',views.get_inventory,name='list_inventory'),
+]
